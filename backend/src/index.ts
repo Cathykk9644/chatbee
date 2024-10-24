115d5d6942167ba5091c3ad9159bfafff5133ca0
Cathy Hui <erashine414@icloud.com>
2024-10-24 14:39:49 +0800

    Initial postgres & prisma setup for database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
